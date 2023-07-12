a19b0a5b54031491851960c342a4ef7b7fa93473
sweets2 <47065131+sweets2@users.noreply.github.com>
2023-07-12 13:24:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from flask import Flask
-from weather_script import get_weather_forecast, check_thunderstorm_forecast
+from flask import Flask, request, render_template, jsonify
+from weather_script import Weather
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    get_weather_forecast()
-    return check_thunderstorm_forecast()
+def home():
+    instance = Weather()
+    return instance.check_thunderstorm_forecast()
+
+
+@app.route('/result', methods = ['POST', 'GET'])
+def result():
+    if request.method == 'GET':
+        result = request.form
+        return render_template("get_location.html", result = result)
+    elif request.method == 'POST':
+        data = request.get_json()
+        latitude = data.get('latitude')
+        return render_template("get_location.html", data = data)
+#        longitude = data.get('longitude')
+#        return f'Latitude: {latitude}, Longitude: {longitude}'
+        #return jsonify({'status': 'success'}), 200
+
+
+@app.route('/location', methods=['POST', 'GET'])
+def location():
+    data = request.get_json()
+    latitude = data.get('latitude')
+    longitude = data.get('longitude')
+    print(f'Latitude: {latitude}, Longitude: {longitude}')
+    return {'status': 'success'}, 200
